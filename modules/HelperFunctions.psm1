0139ac8ed29a95a94b97d3a71c9ac9044699e42e
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-04-14 19:37:58 +0200

    added function

--- modules/HelperFunctions.psm1
+++ modules/HelperFunctions.psm1
@@ -1,27 +1,98 @@
 function YamlTo-Arm {
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory = $true)]
-    [string]$FilesPath,
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$FilesPath,
 
-    [Parameter(Mandatory = $true)]
-    [string]$OutputPath
-    
-)
+        [Parameter(Mandatory = $true)]
+        [string]$OutputPath
+        
+    )
 
-#Region Install Modules
-$modulesToInstall = @(
-    'powershell-yaml'
-)
+    #Region Install Modules
+    $modulesToInstall = @(
+        'powershell-yaml'
+    )
 
-$modulesToInstall | ForEach-Object {
-    if (-not (Get-Module -ListAvailable -All $_)) {
-        Write-Output "Module [$_] not found, INSTALLING..."
-        Install-Module $_ -Force
-        Import-Module $_ -Force
+    $modulesToInstall | ForEach-Object {
+        if (-not (Get-Module -ListAvailable -All $_)) {
+            Write-Output "Module [$_] not found, INSTALLING..."
+            Install-Module $_ -Force
+            Import-Module $_ -Force
+        }
     }
+    #EndRegion Install Modules
+
+    #Region Fetching AlertRules
+    try {
+        $analyticsRules = Get-ChildItem -Path $FilesPath -Include "*.yaml", "*.yml" -Recurse -ErrorAction 'Stop'
+    }
+    catch {
+        Write-Error $_.Exception.Message
+        break
+    }
+    #EndRegion Fetching AlertRules
+
+    #Region Processing AlertRules
+    if ($null -ne $analyticsRules) {
+        foreach ($rule in $analyticsRules) {
+            try {
+                $ruleObject = get-content $rule | ConvertFrom-Yaml
+
+                switch ($ruleObject.kind) {
+                    "MicrosoftSecurityIncidentCreation" {  
+                        $body = @{
+                            "kind"       = "MicrosoftSecurityIncidentCreation"
+                            "properties" = @{
+                                "enabled"       = "true"
+                                "productFilter" = $ruleObject.productFilter
+                                "displayName"   = $ruleObject.displayName
+                            }
+                        }
+                    }
+                    "Scheduled" {
+                        $body = [pscustomobject]@{
+                            "kind"       = "Scheduled"
+                            "properties" = @{
+                                "displayName"              = $ruleObject.name
+                                "description"              = $ruleObject.description
+                                "severity"                 = $ruleObject.severity
+                                "enabled"                  = $true
+                                "query"                    = $ruleObject.query
+                                "queryFrequency"           = ConvertTo-ISO8601 $ruleObject.queryFrequency
+                                "queryPeriod"              = ConvertTo-ISO8601 $ruleObject.queryPeriod
+                                "triggerOperator"          = Convert-TriggerOperator $ruleObject.triggerOperator
+                                "triggerThreshold"         = $ruleObject.triggerThreshold
+                                "suppressionDuration"      = "PT5H"  #Azure Sentinel requires a value here, although suppression is disabled
+                                "suppressionEnabled"       = $false
+                                "tactics"                  = $ruleObject.tactics
+                                "techniques"               = $ruleObject.relevantTechniques
+                                "alertRuleTemplateName"    = $ruleObject.id
+                                "entityMappings"           = $ruleObject.entityMappings
+                                "incidentConfiguration"    = $ruleObject.incidentConfiguration
+                                "sentinelEntitiesMappings" = $ruleObject.sentinelEntitiesMappings
+                                "eventGroupingSettings"    = $ruleObject.eventGroupingSettings
+                                "templateVersion"          = $ruleObject.version
+                            }
+                        }
+                        
+                        # Update duration to ISO8601
+                        if ($null -ne $ruleObject.incidentConfiguration) {
+                            $body.properties.incidentConfiguration.groupingConfiguration.lookbackDuration = ConvertTo-ISO8601 $ruleObject.incidentConfiguration.groupingConfiguration.lookbackDuration
+                        }
+                    }
+                    Default { }
+                }
+            }
+            catch {
+                Write-Error $_.Exception.Message
+                break
+            }
+            ConvertTo-ARM -value $body -outputFile ('{0}/{1}.json' -f ($($rule.DirectoryName), $($rule.BaseName)))
+        }
+    }
+    #EndRegion Processing AlertRules
 }
-#EndRegion Install Modules
 
 #Region HelperFunctions
 function Convert-TriggerOperator {
@@ -95,77 +166,3 @@ function ConvertTo-ARM {
     $template | ConvertTo-Json -Depth 20 | Out-File $outputFile -ErrorAction Stop
 }
 #EndRegion HelperFunctions
-
-#Region Fetching AlertRules
-foreach ($rule in $analyticsRules) {
-
-    try {
-        $analyticsRules = Get-ChildItem -Path $FilesPath -Include "*.yaml", "*.yml" -Recurse -ErrorAction 'Stop'
-    }
-    catch {
-        Write-Error $_.Exception.Message
-        break
-    }
-}
-#EndRegion Fetching AlertRules
-
-#Region Processing AlertRules
-if ($null -ne $analyticsRules) {
-    foreach ($rule in $analyticsRules) {
-        try {
-            $ruleObject = get-content $rule | ConvertFrom-Yaml
-
-            switch ($ruleObject.kind) {
-                "MicrosoftSecurityIncidentCreation" {  
-                    $body = @{
-                        "kind"       = "MicrosoftSecurityIncidentCreation"
-                        "properties" = @{
-                            "enabled"       = "true"
-                            "productFilter" = $ruleObject.productFilter
-                            "displayName"   = $ruleObject.displayName
-                        }
-                    }
-                }
-                "Scheduled" {
-                    $body = [pscustomobject]@{
-                        "kind"       = "Scheduled"
-                        "properties" = @{
-                            "displayName"              = $ruleObject.name
-                            "description"              = $ruleObject.description
-                            "severity"                 = $ruleObject.severity
-                            "enabled"                  = $true
-                            "query"                    = $ruleObject.query
-                            "queryFrequency"           = ConvertTo-ISO8601 $ruleObject.queryFrequency
-                            "queryPeriod"              = ConvertTo-ISO8601 $ruleObject.queryPeriod
-                            "triggerOperator"          = Convert-TriggerOperator $ruleObject.triggerOperator
-                            "triggerThreshold"         = $ruleObject.triggerThreshold
-                            "suppressionDuration"      = "PT5H"  #Azure Sentinel requires a value here, although suppression is disabled
-                            "suppressionEnabled"       = $false
-                            "tactics"                  = $ruleObject.tactics
-                            "techniques"               = $ruleObject.relevantTechniques
-                            "alertRuleTemplateName"    = $ruleObject.id
-                            "entityMappings"           = $ruleObject.entityMappings
-                            "incidentConfiguration"    = $ruleObject.incidentConfiguration
-                            "sentinelEntitiesMappings" = $ruleObject.sentinelEntitiesMappings
-                            "eventGroupingSettings"    = $ruleObject.eventGroupingSettings
-                            "templateVersion"          = $ruleObject.version
-                        }
-                    }
-                    
-                    # Update duration to ISO8601
-                    if ($null -ne $ruleObject.incidentConfiguration) {
-                        $body.properties.incidentConfiguration.groupingConfiguration.lookbackDuration = ConvertTo-ISO8601 $ruleObject.incidentConfiguration.groupingConfiguration.lookbackDuration
-                    }
-                }
-                Default { }
-            }
-        }
-        catch {
-            Write-Error $_.Exception.Message
-            break
-        }
-        ConvertTo-ARM -value $body -outputFile ('{0}/{1}.json' -f ($($rule.DirectoryName), $($rule.BaseName)))
-    }
-}
-#EndRegion Processing AlertRules
-}
